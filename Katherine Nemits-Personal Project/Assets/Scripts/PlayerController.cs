bc824cb75de139d89c67699d1a59a9018535adbf
Katherine Nemits <kn00772@srcs.loc>
2020-02-06 15:04:00 -0800

    Lab 3 Complete

--- Katherine Nemits-Personal Project/Assets/Scripts/PlayerController.cs
+++ Katherine Nemits-Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //Keeps player from going off of right side of screen
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+
+    void MovePlayer()
+    {
+        //Moves player left and right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
