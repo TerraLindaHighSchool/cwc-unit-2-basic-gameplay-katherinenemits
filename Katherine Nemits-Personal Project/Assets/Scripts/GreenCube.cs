66d5eb621bead01e6117c77c9b831114841dd6fb
Katherine Nemits <kn00772@srcs.loc>
2020-02-25 15:09:39 -0800

    Personal Project Unit 4 In Progress

---
+++ Katherine Nemits-Personal Project/Assets/Scripts/GreenCube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenCube : MonoBehaviour
+{
+    int xRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRange = GameObject.getX + 2;
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
